bef3456c8e26feae26be8e8a02627828601d90ec
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-23 14:46:12 -0800

    set up stdin for play.js, able to exit

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
+const { stdin } = require('process');
 const { connect } = require('./client')
 // establishes a connection with the game server
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (input) => {
+  if (input === '\u0003') {
+    process.exit();
+  }
+}
+
+stdin.on('data', handleUserInput);
 
 console.log("Connecting ...");
 connect();
+setupInput();
