2e8c4584f5dd6153667caa9310bfb5342822f051
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-23 13:51:35 -0800

    updated to set snake name

--- client.js
+++ client.js
@@ -1,17 +1,21 @@
 const net = require("net");
 
-const connect = function (data) {
+const connect = (data) => {
   const conn = net.createConnection({
-    host: '172.31.239.24',
-    port: 50541
+    host: "172.31.239.24",
+    port: 50541,
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully Connected to Game Server.");
+    conn.write('Name: DON');
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  conn.on('data', (data) => {
+  conn.on("data", (data) => {
     console.log(data);
-  })
+  });
   return conn;
 };
 
-module.exports = { connect }
+module.exports = { connect };
